be4ffd2f38b82d1aef3d3ba1bc1fadb673d6eb02
Cait Friedlander <caitfinchart@gmail.com>
2016-10-08 23:02:19 -0700

    make start button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 console.log("snek")
+
+//play button function that launches the game screen
+$(function(){
+    $("#gameScreen").hide();
+    $("#playButton").on("click", function(){
+        $("#startScreen, #gameScreen").toggle();
+    });
+});
+
+//function to spawn a grid of divs for the game board
+//use the array of arrays like cartesian coordinates to build a
+//grid of divs
+// var x = [ ['','','','','','','','','','','','','','','','','','','','','',''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['',x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,''],
+//       ['','','','','','','','','','','','','','','','','','','','','',''] ]
+
+//function to spawn the apple randomly using the x and y grid
