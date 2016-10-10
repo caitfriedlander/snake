41ac083e3570b88e5e6ca2cc8f47d030b03f40c3
Cait Friedlander <caitfinchart@gmail.com>
2016-10-10 15:17:37 -0700

    make snake head and apple spawn randomly at the beginning of the game

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-console.log("snek")
+console.log("snek");
 //var y = grid[i]
 //var x = grid[i][i]
 //play button function that launches the game screen
-var snakeArray = [[1,2],
-                  [1,1],
-                  [1,3],
-                  [2,3]]
+// var snake = [[2,3],
+//              [2,4],
+//              [2,5],
+//              [2,6],
+//              [2,7],
+//              [1,7]]
+var snake = [];
+var apple = [];
 
-//buttons!!
+//BUTTONS
 $(function(){
     $("#gameScreen").hide();
     $("#pauseButton").hide();
@@ -17,10 +21,12 @@ $(function(){
         //function to spawn the apple randomly using the x and y grid
         //function to make the starter snake head appear in the same spot
         //on start every time
+        fillSnake();
+        fillApple();
     });
 });
 
-//function to spawn a grid of divs for the game board
+//GRID
 $(document).ready(function() {
     for(var x = 0; x < 37; x++) {
         for(var y = 0; y < 37; y++) {
@@ -30,16 +36,57 @@ $(document).ready(function() {
     }
 });
 
+//SNAKE
+$(document).ready(function generateSnake() {
+    var snakeHead = [];
+    function getRandomX(min, max) {
+      min = Math.ceil(0);
+      max = Math.floor(37);
+      return Math.floor(Math.random() * (max - min)) + min;
+    };
+    snakeHead.push(getRandomX());
+    function getRandomY(min, max) {
+      min = Math.ceil(0);
+      max = Math.floor(37);
+      return Math.floor(Math.random() * (max - min)) + min;
+    };
+    snakeHead.push(getRandomY());
+    snake.push(snakeHead);
+})
+
 function fillSnake() {
-    for (var i=0; i < snakeArray.length; i++) {
-        var row = snakeArray[i][0];
-        var column = snakeArray[i][1];
-        var $currentTile = $('.space')[37 * row + column];
-        // $currentTile.css("background-color","black");
-        $currentTile.addClass("snake");
+    $(".space").css("background-color", "#EAEEE6")
+    for (var i=0; i < snake.length; i++) {
+        var row = snake[i][0];
+        var column = snake[i][1];
+        var currentSpace = (37 * row + column);
+        $('.space').eq(currentSpace).css("background-color","black");
     }
 }
-fillSnake()
+
+//APPLE
+$(document).ready(function generateApple() {
+    function getRandomX(min, max) {
+      min = Math.ceil(0);
+      max = Math.floor(37);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+    apple.push(getRandomX())
+    function getRandomY(min, max) {
+      min = Math.ceil(0);
+      max = Math.floor(37);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+    apple.push(getRandomY())
+})
+
+function fillApple() {
+        var row = apple[0];
+        var column = apple[1];
+        var currentSpace = (37 * row + column);
+        $('.space').eq(currentSpace).css("background-color","red");
+}
+
 
 //function to move the cube around the grid using the arrow keys
 //make all cubes attached to the snake follow the same motion as the head
