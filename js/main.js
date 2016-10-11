50223954bdf5a1c9fb38335ca12275f428d85956
Cait Friedlander <caitfinchart@gmail.com>
2016-10-11 16:45:01 -0700

    fix interval function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var snake = [];
 var apple = [];
 var newSnakeHead;
+snake.direction = null;
+
 //BUTTONS
 $(function(){
     $("#gameScreen").hide();
@@ -96,22 +98,107 @@ function fillSnake() {
 }
 
 //loop through snake array
-var checkSnake = function () {
+function checkSnake () {
   for (var i = 1; i<snake.length; i++) {
     if (_.isEqual(snake[0], snake[i])) {
-      return(true);
-    }
-    else {
-      return(false);
+      $(" #gameScreen, #gameOver").toggle();
+      $("#pauseButton, #restart").toggle();
+      console.log("totally f'ed")
     }
   }
 }
-checkSnake();
+
+//Snake Animation
+var int = setInterval( function() {
+  var snakeHead = snake[0];
+  if (snake.direction === "left") {
+    var newHead = [snakeHead[0], snakeHead[1]-1]
+    if (newHead[0] === apple[0] && newHead[1] === apple[1]) {
+      snake.unshift(apple);
+            //still not working right
+            $(snake).eq(0).css("background-color","black");
+            generateApple();
+            growScore();
+          }
+          else if (newHead[1] < 0) {
+            $(" #gameScreen, #gameOver").toggle();
+            $("#pauseButton, #restart").toggle();
+          }
+          else {
+            snake.unshift(newHead);
+            snake.pop();
+            fillSnake();
+            fillApple();
+          }
+        }
+        if (snake.direction === "up") {
+          var newHead = [snakeHead[0]-1, snakeHead[1]];
+          if (newHead[0] === apple[0] && newHead[1] === apple[1]) {
+            snake.unshift(apple);
+        //still not working right
+        $(snake).eq(0).css("background-color","black");
+        generateApple();
+        growScore();
+      }
+      else if (newHead[0] < 0) {
+        $(" #gameScreen, #gameOver").toggle();
+        $("#pauseButton, #restart").toggle();
+      }
+      else {
+        snake.unshift(newHead);
+        snake.pop();
+        fillSnake();
+        fillApple();
+      }
+    }
+    if (snake.direction === "right") {
+      var newHead = [snakeHead[0], snakeHead[1]+1];
+      if (newHead[0] === apple[0] && newHead[1] === apple[1]) {
+        snake.unshift(apple);
+      //still not working right
+      $(snake).eq(0).css("background-color","black");
+      generateApple();
+      growScore();
+    }
+    else if (newHead[1] > 36) {
+      $(" #gameScreen, #gameOver").toggle();
+      $("#pauseButton, #restart").toggle();
+    }
+    else {
+      snake.unshift(newHead);
+      snake.pop();
+      fillSnake();
+      fillApple();
+    }
+  }
+  if (snake.direction === "down") {
+    var newHead = [snakeHead[0]+1, snakeHead[1]];
+    if (newHead[0] === apple[0] && newHead[1] === apple[1]) {
+      snake.unshift(apple);
+      //still not working right
+      $(snake).eq(0).css("background-color","black");
+      generateApple();
+      growScore();
+    }
+    else if (newHead[0] > 36) {
+      $(" #gameScreen, #gameOver").toggle();
+      $("#pauseButton, #restart").toggle();
+    }
+    else {
+      snake.unshift(newHead);
+      snake.pop();
+      fillSnake();
+      fillApple();
+    }
+  }
+},300);
+
 
 //move and grow snake
 $(document).keydown(function(e) {
     switch(e.which) { // snake.direction
         case 37: // left
+        snake.direction = "left";
         var snakeHead = snake[0];
         var newHead = [snakeHead[0], snakeHead[1]-1];
         if (newHead[0] === apple[0] && newHead[1] === apple[1]) {
@@ -134,6 +221,7 @@ $(document).keydown(function(e) {
         break;
 
         case 38: // up
+        snake.direction = "up";
         var snakeHead = snake[0];
         var newHead = [snakeHead[0]-1, snakeHead[1]];
         if (newHead[0] === apple[0] && newHead[1] === apple[1]) {
@@ -156,6 +244,7 @@ $(document).keydown(function(e) {
         break;
 
         case 39: // right
+        snake.direction = "right";
         var snakeHead = snake[0];
         var newHead = [snakeHead[0], snakeHead[1]+1];
         if (newHead[0] === apple[0] && newHead[1] === apple[1]) {
@@ -178,6 +267,7 @@ $(document).keydown(function(e) {
         break;
 
         case 40: // down
+        snake.direction = "down";
         var snakeHead = snake[0];
         var newHead = [snakeHead[0]+1, snakeHead[1]];
         if (newHead[0] === apple[0] && newHead[1] === apple[1]) {
